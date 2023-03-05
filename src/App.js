0137a96e4dcc6f6b0a0ca8f90eecf3383a6ed8b1
Simran Mehta <mehtasimran234@gmail.com>
2023-03-05 23:41:49 +0530

    Challenge 3.1: Local state

--- src/App.js
+++ src/App.js
@@ -3,17 +3,6 @@ import boxes from "./boxes"
 import Box from "./Box"
 
 function App() {
-    /**
-     * Challenge part 2:
-     * 1. Create a separate component called "Box" and
-     *    replace the `div` above with our <Box /> components
-     * 2. Pass the Box component a prop called `on` with the
-     *    value of the same name from the `boxes` objects
-     * 3. In the Box component, apply dynamic styles to determine
-     *    the backgroundColor of the box. If it's `on`, set the
-     *    backgroundColor to "#222222". If off, set it to "transparent"
-     */
-
   const [squareArray, setSquareArray] = React.useState(boxes)
 
   const items = squareArray.map(square => {
--- src/Box.js
+++ src/Box.js
@@ -1,11 +1,29 @@
 import React from "react";
 
 export default function Box(props) {
+    /**
+     * Challenge part 3.1: 
+     * Create state controlling whether
+     * this box is "on" or "off". Use the incoming
+     * `props.on` to determine the initial state.
+     * 
+     * Create an event listener so when the box is clicked,
+     * it toggles from "on" to "off".
+     * 
+     * Goal: clicking each box should toggle it on and off.
+     */
+
+    const [on, setOn] = React.useState(props.on)
+
+    function handleClick() {
+        setOn(prevOn => !prevOn)
+    }
+
     const styles = {
-        backgroundColor: props.on ? "#222222" : "transparent"
+        backgroundColor: on ? "#222222" : "transparent"
     }
 
     return (   
-      <div className="box" style={styles}></div>
+      <div className="box" style={styles} onClick={handleClick}></div>
     )
 }
