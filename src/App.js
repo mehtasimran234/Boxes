faeeffce81ec6356a094e349f189fa36a571da76
Simran Mehta <mehtasimran234@gmail.com>
2023-03-05 23:29:56 +0530

    Challenge 2: Create Box component with prop

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from "react";
 import boxes from "./boxes"
+import Box from "./Box"
 
 function App() {
-     /**
-     * Challenge part 1:
-     * 1. Initialize state with the default value of the
-     *    array pulled in from boxes.js
-     * 2. Map over that state array and display each one
-     *    as an empty square (black border, transparent bg color)
-     *    (Don't worry about using the "on" property yet)
+    /**
+     * Challenge part 2:
+     * 1. Create a separate component called "Box" and
+     *    replace the `div` above with our <Box /> components
+     * 2. Pass the Box component a prop called `on` with the
+     *    value of the same name from the `boxes` objects
+     * 3. In the Box component, apply dynamic styles to determine
+     *    the backgroundColor of the box. If it's `on`, set the
+     *    backgroundColor to "#222222". If off, set it to "transparent"
      */
 
   const [squareArray, setSquareArray] = React.useState(boxes)
 
   const items = squareArray.map(square => {
     return (
-      <div className="box" key={square.id}></div>
+      <Box key={square.id} on={square.on} />
     )
   })
 
