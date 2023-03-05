ddf5ab0d6b81e793546407e2a1bb02507a54c1b6
Simran Mehta <mehtasimran234@gmail.com>
2023-03-05 23:15:49 +0530

    Challenge 1: Initialize state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
+import React from "react";
+import boxes from "./boxes"
+
 function App() {
+     /**
+     * Challenge part 1:
+     * 1. Initialize state with the default value of the
+     *    array pulled in from boxes.js
+     * 2. Map over that state array and display each one
+     *    as an empty square (black border, transparent bg color)
+     *    (Don't worry about using the "on" property yet)
+     */
+
+  const [squareArray, setSquareArray] = React.useState(boxes)
+
+  const items = squareArray.map(square => {
+    return (
+      <div className="box" key={square.id}></div>
+    )
+  })
+
   return (
     <div className="App">
+      {items}
     </div>
   );
 }
