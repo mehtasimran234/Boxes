927e5e7bf8aade90ac70a33c42c11fae5e03aa83
Simran Mehta <mehtasimran234@gmail.com>
2023-03-05 23:46:46 +0530

    Challenge 3.2: Unified state

--- src/App.js
+++ src/App.js
@@ -3,11 +3,25 @@ import boxes from "./boxes"
 import Box from "./Box"
 
 function App() {
+
+    /**
+     * Challenge part 3.2:
+     * Create a toggle() function that logs
+     * "clicked!" to the console
+     * 
+     * Pass that function down to each of the Box components
+     * and set it up so when they get clicked it runs the function
+     */
+
   const [squareArray, setSquareArray] = React.useState(boxes)
+
+  function toggle() {
+    console.log("Clicked!!")
+  }
 
   const items = squareArray.map(square => {
     return (
-      <Box key={square.id} on={square.on} />
+      <Box key={square.id} on={square.on} handleClick={toggle} />
     )
   })
 
